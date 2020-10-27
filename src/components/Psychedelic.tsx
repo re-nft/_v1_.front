6e568f6975136a895bba9d33fe67016d42e205be
Naz <nazariy@inbox.ru>
2020-10-27 23:42:27 +0000

    [+] pull some fake people

--- src/components/Psychedelic.tsx
+++ src/components/Psychedelic.tsx
@@ -1,32 +1,37 @@
-import React, { useContext, useEffect, useState, useCallback } from "react";
+import React, { useContext, useEffect, useState, useCallback, useMemo } from "react";
 
 import ScrollForMore from "../components/ScrollForMore";
 import DappContext from "../contexts/Dapp";
-import { ENDPOINT, nftsQuery } from "../api/graph";
+import { ENDPOINT, nftsQuery, userQuery } from "../api/graph";
 import { request } from "graphql-request";
 
-export const Catalogue = ({data, btnActionLabel}) => (
-  <div className="Catalogue">
-  {data.allCustomApi.edges[0].node.assets.map((product) => {
+export const Catalogue = ({data, btnActionLabel}) => {
+  const resolved = useMemo(() => {
+    if (btnActionLabel === "Lend") {
+      return data.user.faces;
+    }
+  }, [data, btnActionLabel]);
+
+  return (<div className="Catalogue">
+  {resolved.map((face) => {
     return (
-      <div className="Catalogue__item" key={product.token_id}>
+      <div className="Catalogue__item" key={face.id}>
         <div
           className="Product snipcart-add-item"
-          data-item-id={product.token_id}
-          data-item-price="10"
-          data-item-image={product.image_url}
+          data-item-id={face.id}
+          data-item-image={face.uri}
           data-item-name="a"
           data-item-url={`/`}
         >
           <div className="Product__image">
-            <img alt="nft" src={product.image_original_url} />
+            <img alt="nft" src={face.uri} />
             {/* <Img key={product.token_id} fluid /> */}
             {/* <Img sizes={product.image.sizes} /> */}
           </div>
           <div className="Product__details">
             <div className="Product__name">
               {/* {product.asset_contract.address} */}
-              <div className="Product__price">10 €</div>
+              {btnActionLabel === "Rent" && <div className="Product__price">10 €</div>}
             </div>
             <span className="Product__buy">{btnActionLabel} now</span>
           </div>
@@ -34,8 +39,8 @@ export const Catalogue = ({data, btnActionLabel}) => (
       </div>
     );
   })}
-</div>
-);
+</div>)
+};
 
 type PsychedelicProps = {
   children?: React.ReactNode;
@@ -55,11 +60,24 @@ const Psychedelic: React.FC<PsychedelicProps> = ({ children, hidden, isRent }) =
   const [data, setData] = useState();
   const { wallet, web3 } = useContext(DappContext);
   const fetchNfts = useCallback(async () => {
-    const data = await request(ENDPOINT, nftsQuery());
-    if ('nfts' in data && data['nfts'].length !== 0) {
-      setData(data['nfts']);
+    if (isRent) {
+      const data = await request(ENDPOINT, nftsQuery());
+      if ('nfts' in data && data['nfts'].length !== 0) {
+        setData(data['nfts']);
+      }
+    } else {
+      // lend. so pull all the nfts that you own
+      if (wallet.account == null || web3 == null) {
+        console.error("connect to goerli network");
+        return;
+      }
+      const userNftsQuery = userQuery(wallet.account, web3);
+      const data = await request(ENDPOINT, userNftsQuery);
+      console.log("data");
+      console.log(data);
+      setData(data);
     }
-  }, []);
+  }, [wallet.account, web3]);
 
   useEffect(() => {
     fetchNfts();
