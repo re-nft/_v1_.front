8417c421dd6b7898b67529e06e143362a9cb6e42
Naz <nazariy@inbox.ru>
2020-10-27 22:23:07 +0000

    [+] fixed the production build

---
+++ src/api/graph.ts
@@ -0,0 +1,13 @@
+const ENDPOINT = "https://api.thegraph.com/subgraphs/name/nazariyv/rentnft";
+
+const facesQuery = "";
+
+const nftsQuery = "";
+
+const userQuery = "";
+
+// const getProduct = async () => {
+//     const nftInfo = await request(endpoint, productQuery(nftId));
+//     setProduct(nftInfo.product);
+//     console.log(nftInfo.product);
+//   };
