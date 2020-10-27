6848e5373fdbe543a630c2a824cae36621b11313
Naz <nazariy@inbox.ru>
2020-10-27 23:16:35 +0000

    [+] graph nfts pulling

--- src/api/graph.ts
+++ src/api/graph.ts
@@ -1,22 +1,23 @@
 import Web3 from "web3";
 
-const ENDPOINT = "https://api.thegraph.com/subgraphs/name/nazariyv/rentnft";
+export const ENDPOINT = "https://api.thegraph.com/subgraphs/name/nazariyv/rentnft";
 
 const facesQuery = "";
 
-const nftsQuery = (): string => {
-    return `nfts(borrower: "null") {
-        id
-        address
-        lender
-        borrower
-        face {
-          uri
-        }
-      }`
+export const nftsQuery = (): string => {
+    return `{
+        nfts {
+          id
+          address
+          lender
+          borrower
+          face {
+            uri
+          }
+      }}`
 }
 
-const userQuery = (user: string, web3: Web3): string => {
+export const userQuery = (user: string, web3: Web3): string => {
     const hex = web3.utils.toHex(user);
     return `{
         user(id: "${hex}") {
--- src/components/Psychedelic.tsx
+++ src/components/Psychedelic.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext, useEffect, useState, useCallback } from "react";
 
 import ScrollForMore from "../components/ScrollForMore";
+import DappContext from "../contexts/Dapp";
+import { ENDPOINT, nftsQuery } from "../api/graph";
+import { request } from "graphql-request";
 
 export const Catalogue = ({data, btnActionLabel}) => (
   <div className="Catalogue">
@@ -41,7 +44,27 @@ type PsychedelicProps = {
   isRent: boolean;
 }
 
-const Psychedelic: React.FC<PsychedelicProps> = ({ children, data, hidden, isRent }) => {
+
+// const getProduct = useCallback(async (): Promise<void> => {
+//   const nftInfo = await request(endpoint, productQuery(nftId));
+//   setProduct(nftInfo.product);
+//   console.log(nftInfo.product);
+// }, [nftId]);
+
+const Psychedelic: React.FC<PsychedelicProps> = ({ children, hidden, isRent }) => {
+  const [data, setData] = useState();
+  const { wallet, web3 } = useContext(DappContext);
+  const fetchNfts = useCallback(async () => {
+    const data = await request(ENDPOINT, nftsQuery());
+    if ('nfts' in data && data['nfts'].length !== 0) {
+      setData(data['nfts']);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchNfts();
+  }, [wallet.account, web3]);
+
   const btnActionLabel = isRent ? "Rent" : "Lend";
 
   return (
