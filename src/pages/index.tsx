35328718371dde106a00d9c891b7bb1a30fa1eff
nazariyv <nazariy@inbox.ru>
2020-11-09 14:28:08 +0000

    [+] consistent font everywhere

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { UseWalletProvider } from "use-wallet";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 // contexts
 import { DappProvider } from "../contexts/Dapp";
@@ -10,6 +11,18 @@ import { GraphProvider } from "../contexts/Graph";
 // components and other
 import App from "../components/App";
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Righteous",
+      "consolas",
+      "Menlo",
+      "monospace",
+      "sans-serif",
+    ].join(","),
+  },
+});
+
 const Index: React.FC = () => {
   return (
     <UseWalletProvider chainId={5}>
@@ -17,7 +30,9 @@ const Index: React.FC = () => {
         <GraphProvider>
           <ContractsProvider>
             <GanFacesProvider>
-              <App />
+              <ThemeProvider theme={theme}>
+                <App />
+              </ThemeProvider>
             </GanFacesProvider>
           </ContractsProvider>
         </GraphProvider>
