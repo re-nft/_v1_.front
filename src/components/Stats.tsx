01bfc44360cd2ec52ba4a6a0a0585f90bf44fefa
nazariyv <nazariy@inbox.ru>
2020-11-10 12:12:54 +0000

    [+] added stats table skeleton

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Box, Tooltip } from "@material-ui/core";
 
 import ProgressBar from "./Progressbar";
+import Table from "./Table";
 
 type StatsProps = {
   hidden: boolean;
@@ -12,57 +14,120 @@ const Stats: React.FC<StatsProps> = ({ hidden }) => {
   }
 
   return (
-    <div>
-      <div className="coming-soon" title="Coming Soon,">
-        Coming Soon,
-      </div>
-      <div className="coming-soon" title="it will look">
-        it will look
-      </div>
-      <div className="coming-soon" title="fab">
-        fab
-      </div>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <div>Total earned from lending: $1222 (in rent prices)</div>
-        <div># of NFTs currently lending: 3</div>
-        <div># of NFTS currently renting: 1</div>
-        <div>Earning: dynamic numbers</div>
-        <div>Spending: dynamic numbers</div>
+    <Box style={{ display: "flex", flexDirection: "column" }}>
+      <div>Total earned from lending: $1222 (in rent prices)</div>
+      <div># of NFTs currently lending: 3</div>
+      <div># of NFTS currently renting: 1</div>
+      <div>Earning: dynamic numbers</div>
+      <div>Spending: dynamic numbers</div>
 
-        <div>------------------</div>
-        <div>Currently Lending</div>
-        <div>------------------</div>
-        <div style={{ display: "flex" }}>
-          <div>
-            1. 0x423...455/234 @ $10 per day | day: 1 | earned: $3 &nbsp;{" "}
-          </div>
-          <div>
-            <ProgressBar />
-          </div>
-        </div>
-        <div style={{ display: "flex" }}>
-          {" "}
-          2. 0xf19...dcd/1 @ $5 per day | day: 10 | earned: $51 &nbsp;{" "}
-          <ProgressBar />
-        </div>
-        <div style={{ display: "flex" }}>
-          {" "}
-          3. 0xaa2...ba8/21 @ $3 per day | day: 5 | earned: $16 &nbsp;{" "}
-          <ProgressBar />
-        </div>
-        <div>------------------</div>
-        <div>Currently Renting</div>
-        <div>------------------</div>
-        <div style={{ display: "flex" }}>
-          <div>
-            1. 0xaaa...ddd/1 @ $100 per day | day: 1 | spent: $55 &nbsp;{" "}
-          </div>
-          <div>
-            <ProgressBar />
-          </div>
-        </div>
-      </div>
-    </div>
+      <br />
+      <div>------------------</div>
+      <div>LENDING</div>
+      <div>------------------</div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Preview</th>
+            <th>NFT Address</th>
+            <th>Token id</th>
+            <th>Daily borrow price</th>
+            <th>Collateral</th>
+            <th>Max Duration</th>
+            <th>Actual Duration</th>
+            <th>Earned</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>[]</td>
+            <td>0xkjkj3k2423</td>
+            <td>0x4</td>
+            <td>$100</td>
+            <td>$1000</td>
+            <td>20</td>
+            <td>10</td>
+            {/* TODO: tooltip will show what day this is */}
+            <td>$50/$100</td>
+          </tr>
+          <tr>
+            <td>[]</td>
+            <td>0xkjkj3k2423</td>
+            <td>0x4</td>
+            <td>$100</td>
+            <td>$1000</td>
+            <td>20</td>
+            <td>10</td>
+            {/* TODO: tooltip will show what day this is */}
+            <td>$50/$100</td>
+          </tr>
+          <tr>
+            <td>[]</td>
+            <td>0xkjkj3k2423</td>
+            <td>0x4</td>
+            <td>$100</td>
+            <td>$1000</td>
+            <td>20</td>
+            <td>-</td>
+            {/* TODO: tooltip will show what day this is */}
+            <td>-</td>
+          </tr>
+        </tbody>
+      </Table>
+
+      <br />
+      <div>------------------</div>
+      <div>BORROWING</div>
+      <div>------------------</div>
+      <Table />
+      <thead>
+        <tr>
+          <th>Preview</th>
+          <th>NFT Address</th>
+          <th>Token id</th>
+          <th>Daily borrow price</th>
+          <th>Collateral</th>
+          <th>Max Duration</th>
+          <th>Actual Duration</th>
+          <th>Earned</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>[]</td>
+          <td>0xkjkj3k2423</td>
+          <td>0x4</td>
+          <td>$100</td>
+          <td>$1000</td>
+          <td>20</td>
+          <td>10</td>
+          {/* TODO: tooltip will show what day this is */}
+          <td>$50/$100</td>
+        </tr>
+        <tr>
+          <td>[]</td>
+          <td>0xkjkj3k2423</td>
+          <td>0x4</td>
+          <td>$100</td>
+          <td>$1000</td>
+          <td>20</td>
+          <td>10</td>
+          {/* TODO: tooltip will show what day this is */}
+          <td>$50/$100</td>
+        </tr>
+        <tr>
+          <td>[]</td>
+          <td>0xkjkj3k2423</td>
+          <td>0x4</td>
+          <td>$100</td>
+          <td>$1000</td>
+          <td>20</td>
+          <td>-</td>
+          {/* TODO: tooltip will show what day this is */}
+          <td>-</td>
+        </tr>
+      </tbody>
+    </Box>
   );
 };
 
