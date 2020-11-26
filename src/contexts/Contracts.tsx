fb37d2e7d789993a0e70c4229a7e4ebfc00d0d78
nazariyv <nazariy@inbox.ru>
2020-11-26 19:26:35 +0000

    [*] bunch of re-factors

--- src/contexts/Contracts.tsx
+++ src/contexts/Contracts.tsx
@@ -5,19 +5,21 @@ import React, {
   useCallback,
   useContext,
 } from "react";
+import { AbiItem } from "web3-utils";
 import { Contract } from "web3-eth-contract";
 
 import DappContext from "./Dapp";
-import { abis, addresses } from "../contracts";
-import { Address, Lending, PaymentToken } from "../types";
+import { Address, PaymentToken } from "../types";
 import { MAX_UINT256 } from "../constants";
 import { THROWS } from "../utils";
+import Web3 from "web3";
+import addresses from "contracts/addresses";
 
 type ContractsContextType = {
   helpers: {
-    getOpenSeaNfts: () => Lending[];
-    setExternalNfts: () => Promise<void>;
-    getExternalNfts: () => Promise<void>;
+    getOpenSeaNfts: () => void;
+    setExternalNfts: () => void;
+    getExternalNfts: () => void;
   };
   erc20: {
     contract?: Contract;
@@ -29,9 +31,9 @@ type ContractsContextType = {
   };
   face: {
     contract?: Contract;
-    isApproved: (tokenId: string, whichOperator: Address) => Promise<boolean>;
-    approve: (tokenId: string, operator: Address) => void;
-    approveAll: (operator: Address) => void;
+    isApproved: (tokenId: string) => Promise<boolean>;
+    approve: (tokenId: string) => void;
+    approveAll: () => void;
   };
   rent: {
     contract?: Contract;
@@ -115,8 +117,22 @@ type ContractsContextType = {
 };
 
 const DefaultContractsContext = {
+  helpers: {
+    getOpenSeaNfts: THROWS,
+    setExternalNfts: THROWS,
+    getExternalNfts: THROWS,
+  },
+  erc20: {
+    approve: THROWS,
+  },
+  erc721: {
+    approve: THROWS,
+  },
   face: {
-    isApproved: THROWS,
+    isApproved: async () => {
+      console.error("must be implemented");
+      return false;
+    },
     approve: THROWS,
     approveAll: THROWS,
   },
@@ -145,135 +161,89 @@ type ContractsProviderProps = {
 export const ContractsProvider: React.FC<ContractsProviderProps> = ({
   children,
 }) => {
-  const { web3, wallet } = useContext(DappContext);
+  const { web3, wallet, addresses, abis } = useContext(DappContext);
   const [face, setFace] = useState<Contract>();
   const [rent, setRent] = useState<Contract>();
 
-  // checks that there is web3 and wallet
-  // plus any additional args
-  const dappOk = useCallback(
-    (...args) => {
-      if (!web3 || !wallet?.account) {
-        console.debug("no web3 or wallet");
-        return false;
-      }
-      for (const arg of args) {
-        if (!arg) return false;
-      }
-      return true;
+  const getContract = useCallback(
+    (abi: AbiItem, address: Address): Contract | undefined => {
+      if (!web3) return;
+      const contract = new web3.eth.Contract(abi, address);
+      return contract;
     },
-    [web3, wallet]
+    [web3]
   );
-
-  // const getDaiContract = useCallback(async () => {
-  //   if (!dappOk()) return;
-  //   if (dai != null) return;
-
-  //   // todo: checkDapp as typeguard that web3 is not null
-  //   const contract = new web3!.eth.Contract(
-  //     abis.erc20.abi,
-  //     addresses.goerli.dai
-  //   );
-  //   setDai(contract);
-  // }, [web3, dai, dappOk]);
-
-  const getFaceContract = useCallback(async () => {
-    if (!dappOk()) return;
-    if (face != null) return;
-
-    const contract = new web3.eth.Contract(
-      abis.goerli.face.abi,
-      addresses.goerli.face
-    );
-    setFace(contract);
-  }, [web3, face, dappOk]);
 
   const getRentContract = useCallback(async () => {
-    if (!dappOk()) return;
-    if (rent != null) return;
+    if (!abis?.rent || !addresses?.rent) return;
 
-    const contract = new web3.eth.Contract(
-      abis.goerli.rent.abi,
-      addresses.goerli.rent
-    );
+    const contract = getContract(abis.rent, addresses.rent);
+    if (!contract) return;
+
     setRent(contract);
-  }, [web3, rent, dappOk]);
+  }, [getContract, abis?.rent, addresses?.rent]);
 
-  const getAllContracts = useCallback(async () => {
-    await Promise.all([getFaceContract(), getRentContract()]);
-  }, [getFaceContract, getRentContract]);
+  // --------------------------- Face -------------------------------
+  const getFaceContract = useCallback(async () => {
+    if (!abis?.face || !addresses?.face) return;
 
-  useEffect(() => {
-    getAllContracts();
-  }, [getAllContracts]);
+    const contract = getContract(abis.face, addresses.face);
+    if (!contract) return;
 
-  const approveAll = useCallback(async () => {
-    if (!dappOk(face)) return;
+    setFace(contract);
+  }, [getContract, addresses?.face, abis?.face]);
 
-    // todo: checkdapp typeguard against nulls
+  const approveAllFace = useCallback(async () => {
+    if (!web3 || !wallet?.account || !addresses?.rent || !face?.methods) return;
     await face?.methods
-      .setApprovalForAll(addresses.goerli.rent, true)
+      .setApprovalForAll(addresses?.rent, true)
       .send({ from: wallet?.account });
-  }, [face, dappOk, wallet?.account]);
+  }, [face, web3, wallet?.account, addresses?.rent]);
 
-  const approve = useCallback(
-    async (nftAddress: string, tokenId: string, approveWho: Address) => {
-      if (!dappOk(face)) return;
-
-      // todo: checkdapp typeguard against nulls
+  const approveFace = useCallback(
+    async (tokenId: string) => {
+      if (!web3 || !wallet?.account || !addresses?.rent || !face?.methods)
+        return;
       await face?.methods
-        .approve(addresses.goerli.rent, tokenId)
+        .approve(addresses?.rent, tokenId)
         .send({ from: wallet?.account });
     },
-    [face, dappOk, wallet]
+    [face, web3, wallet?.account, addresses?.rent]
   );
 
-  const isApproved = useCallback(
-    async (nftAddress: Address, tokenId: string) => {
-      if (!dappOk(face)) return;
-
-      // todo: checkdapp typeguard against nulls
+  const _isApprovedFace = useCallback(
+    async (tokenId: string) => {
       const account = await face?.methods.getApproved(tokenId).call();
-      return account;
+      return account.toLowerCase() === addresses?.rent.toLowerCase();
     },
-    [face, dappOk]
+    [face, addresses?.rent]
   );
 
-  const isApprovedAll = useCallback(async () => {
-    if (!dappOk(face)) return;
-
-    // todo: checkdapp typeguard against nulls
-    const bool = await face?.methods
-      .isApprovedForAll(wallet?.account, addresses.goerli.rent)
-      .call();
-    return bool;
-  }, [face, dappOk, wallet]);
-
-  // infinite approval of the payment token
-  // const approveDai = useCallback(async () => {
-  //   if (!dappOk(dai)) return;
-
-  //   await dai?.methods
-  //     .approve(addresses.goerli.rent, UNLIMITED_ALLOWANCE)
-  //     .send({ from: wallet?.account });
-  // }, [wallet?.account, dai, dappOk]);
-
-  // ----------------- Contract Interaction -----------------------
+  const isApprovedFace = useCallback(
+    async (tokenId: string) => {
+      let itIs = await face?.methods
+        .isApprovedForAll(wallet?.account, addresses?.rent)
+        .call();
+      if (itIs) return true;
+      itIs = await _isApprovedFace(tokenId);
+      return itIs;
+    },
+    [face?.methods, wallet?.account, addresses?.rent, _isApprovedFace]
+  );
+  // ----------------------------------------------------------------
 
   // rent one NFT
   const rentOne = useCallback(
     async (tokenId: string, rentDuration: string) => {
-      if (!dappOk(rent)) return;
-
       // ! TODO: change for the address of the NFT
-      await rent?.methods
-        .rentOne(
-          wallet?.account,
-          addresses.goerli.face,
-          web3?.utils.hexToNumberString(tokenId),
-          rentDuration
-        )
-        .send({ from: wallet?.account });
+      // await rent?.methods
+      //   .rentOne(
+      //     wallet?.account,
+      //     addresses.goerli.face,
+      //     web3?.utils.hexToNumberString(tokenId),
+      //     rentDuration
+      //   )
+      //   .send({ from: wallet?.account });
     },
     [rent, wallet?.account, dappOk, web3]
   );
@@ -286,25 +256,21 @@ export const ContractsProvider: React.FC<ContractsProviderProps> = ({
       borrowPrice: string,
       nftPrice: string
     ) => {
-      if (!dappOk(rent)) return;
-
-      await rent?.methods
-        .lendOne(
-          addresses.goerli.face,
-          tokenId,
-          maxDuration,
-          borrowPrice,
-          nftPrice
-        )
-        .send({ from: wallet?.account });
+      // await rent?.methods
+      //   .lendOne(
+      //     addresses.goerli.face,
+      //     tokenId,
+      //     maxDuration,
+      //     borrowPrice,
+      //     nftPrice
+      //   )
+      //   .send({ from: wallet?.account });
     },
     [wallet?.account, rent, dappOk]
   );
 
   const returnOne = useCallback(
     async (nftAddress: string, tokenId: string) => {
-      if (!dappOk(rent)) return;
-
       await rent?.methods
         .returnNftOne(nftAddress, tokenId)
         .send({ from: wallet?.account });
@@ -314,23 +280,46 @@ export const ContractsProvider: React.FC<ContractsProviderProps> = ({
 
   // ---------------------------------------------------------------
 
+  const getAllContracts = useCallback(async () => {
+    await Promise.all([getFaceContract(), getRentContract()]);
+    // * all contracts should be re-fetched when network or an account change
+  }, [getFaceContract, getRentContract]);
+
+  useEffect(() => {
+    getAllContracts();
+  }, [getAllContracts, wallet?.networkName, wallet?.account]);
+
   return (
     <ContractsContext.Provider
       value={{
-        pmtToken: {
-          dai: {
-            contract: dai,
-            approve: approveDai,
-          },
-        },
         face: {
           contract: face,
-          approveAll,
-          approve,
-          isApproved,
-          isApprovedAll,
+          isApproved: isApprovedFace,
+          approve: approveFace,
+          approveAll: approveAllFace,
         },
-        rent: { contract: rent, lendOne, rentOne, returnOne },
+        erc20: {
+          // * may fail if the transaction amount is higher than allowance
+          isApproved: isApprovedErc20,
+          approve: approveErc20,
+        },
+        erc721: {
+          isApproved: isApprovedErc721,
+          approve: approveErc721,
+        },
+        rent: {
+          contract: rent,
+          lendOne,
+          lendMultiple,
+          rentOne,
+          rentMultiple,
+          returnOne,
+          returnMultiple,
+          stopLednginOne,
+          stopLendingMultiple,
+          claimCollateralOne,
+          claimCollateralMultiple,
+        },
       }}
     >
       {children}
