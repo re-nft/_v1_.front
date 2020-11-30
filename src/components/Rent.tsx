851907b3a776b09ce4df7155ac7cbe65129a99f5
nazariyv <nazariy@inbox.ru>
2020-11-30 19:04:12 +0000

    [*] updated user query

--- src/components/Rent.tsx
+++ src/components/Rent.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect, useCallback } from "react";
 import { Box } from "@material-ui/core";
 
 import DappContext from "../contexts/Dapp";
-// import GraphContext from "../contexts/Graph";
+import GraphContext from "../contexts/Graph";
 import ContractsContext from "../contexts/Contracts";
 
 import ScrollForMore from "./ScrollForMore";
@@ -17,25 +17,22 @@ type RentProps = {
 
 const Rent: React.FC<RentProps> = ({ hidden }) => {
   const { wallet } = useContext(DappContext);
-  // const { user } = useContext(GraphContext);
+  const { user } = useContext(GraphContext);
   const { helpers } = useContext(ContractsContext);
-  const { fetchOpenSeaNfts } = helpers;
+  const { openSeaNfts, nonOpenSeaNfts } = helpers;
   const [data, setData] = useState<Lending[]>([]);
   const [showIBorrow, setShowIborrow] = useState(false);
 
   const handleShowIBorrow = useCallback(() => {
     setShowIborrow((showIBorrow) => !showIBorrow);
   }, []);
+
   useEffect(() => {
     if (!wallet?.account) {
       console.debug("no wallet or account");
       return;
     }
-    fetchOpenSeaNfts(wallet?.account);
     // ! only pulling NFTs where we are not the lender
-    // and there is no borrower
-    // this query might become heavy eventually (the query behind this data)
-    // and I may need to fetch it in multiple calls
     // const resolvedData = nfts.filter(
     //   (item) =>
     //     item.lender !== wallet.account?.toLowerCase() &&
