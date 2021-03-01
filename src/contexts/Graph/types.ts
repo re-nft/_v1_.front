06e66cc168d9b534a42be1328751d4aacc2a8f59
nazariyv <nazariy@inbox.ru>
2021-03-01 20:32:29 +0000

    [+] updated types

--- src/contexts/Graph/types.ts
+++ src/contexts/Graph/types.ts
@@ -1,20 +1,28 @@
 import { ERC721 } from "../../hardhat/typechain/ERC721";
 import { ERC1155 } from "../../hardhat/typechain/ERC1155";
-import { Address, Nft, PaymentToken, TokenId } from "../../types";
+import { Address, PaymentToken, TokenId } from "../../types";
 
 // todo: a lot of redundancy
 
-export type MyNft = {
+export type Nft = {
+  contract?: ERC721 | ERC1155;
+  isERC721: boolean;
   address: Address;
   tokenId: string;
   tokenURI?: string;
+  meta?: {
+    mediaURI?: string;
+    media?: Blob;
+  };
+  lending?: Lending[];
+  renting?: Renting[];
 };
 
 // raw data that comes from the eip721 subgraph
 export type MyERC721s = {
   tokens: {
-    id: string; // e.g. "0xbcd4f1ecff4318e7a0c791c7728f3830db506c71_3000013"
-    tokenURI?: string; // e.g. "https://nft.service.cometh.io/3000013"
+    tokenId: Nft["tokenId"]; // e.g. "0xbcd4f1ecff4318e7a0c791c7728f3830db506c71_3000013"
+    tokenURI?: Nft["tokenURI"]; // e.g. "https://nft.service.cometh.io/3000013"
   }[];
 };
 
@@ -24,8 +32,8 @@ export type MyERC1155s = {
     balances: {
       amount: number;
       token: {
-        tokenId: string;
-        tokenURI?: string;
+        tokenId: Nft["tokenId"];
+        tokenURI?: Nft["tokenURI"];
         registry: {
           contractAddress: Address;
         };
@@ -34,38 +42,22 @@ export type MyERC1155s = {
   };
 };
 
-// '0x123...456': { tokenIds: { '1': ..., '2': ... } }
+// differently arranged (for efficiency) MyNft
+// '0x123...456': { tokens: { '1': ..., '2': ... } }
 export type AddressToNft = {
   [key: string]: {
-    contract: ERC721 | ERC1155;
-    isERC721: boolean;
-    isApprovedForAll: boolean;
-    tokenIds: {
+    contract: Nft["contract"];
+    isERC721: Nft["isERC721"];
+    tokens: {
+      // tokenId
       [key: string]: {
-        lending: Lending;
-        renting: Renting;
-        meta?: Response;
+        lending?: Nft["lending"];
+        renting?: Nft["renting"];
+        tokenURI?: Nft["tokenURI"];
+        meta?: Nft["meta"];
       };
     };
   };
-};
-
-export type UserData = {
-  lendings: Lending[];
-  rentings: RentingAndLending[];
-};
-
-export type LendingRaw = {
-  id: string;
-  nftAddress: string;
-  tokenId: string;
-  lenderAddress: string;
-  maxRentDuration: string;
-  dailyRentPrice: string;
-  nftPrice: string;
-  paymentToken: string;
-  renting?: string;
-  collateralClaimed: boolean;
 };
 
 export type Lending = {
@@ -81,14 +73,6 @@ export type Lending = {
   collateralClaimed: boolean;
 };
 
-export type RentingRaw = {
-  id: string;
-  renterAddress: string;
-  rentDuration: string;
-  rentedAt: string;
-  lending: string;
-};
-
 export type Renting = {
   id: string;
   renterAddress: Address;
@@ -97,40 +81,14 @@ export type Renting = {
   lendingId: string;
 };
 
-export type LendingRentingRaw = {
-  id: string;
-  lending: LendingRaw[];
-  renting?: RentingRaw[];
-};
-
 export type LendingRenting = {
   id: string;
   lending: Lending[];
-  renting?: Renting[];
-};
-
-export type UserRaw = {
-  id: string;
-  lending?: LendingRaw[];
-  renting?: RentingRaw[];
-};
-
-export type RentingAndLending = Renting & {
-  lending: Lending;
+  renting: Renting[];
 };
 
 export type User = {
-  id: string;
-  lending?: Lending[];
-  renting?: RentingAndLending[];
-};
-
-export type LendingRentInfo = Pick<
-  Lending,
-  "dailyRentPrice" | "maxRentDuration" | "paymentToken" | "nftPrice"
->;
-
-export type NftAndLendingId = Nft & {
-  lendingId: string;
-  lendingRentInfo: LendingRentInfo;
+  address: Address;
+  lending: Lending[];
+  renting: Renting[];
 };
