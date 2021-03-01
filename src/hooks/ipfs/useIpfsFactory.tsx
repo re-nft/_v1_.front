4c4206a7f25788767077b9d5d86c70e1812bfb08
naz <nazariy@inbox.ru>
2021-03-01 16:58:36 +0000

    [+] further clean up in useIpfsFactory

--- src/hooks/ipfs/useIpfsFactory.tsx
+++ src/hooks/ipfs/useIpfsFactory.tsx
@@ -1,15 +1,17 @@
 import Ipfs from "ipfs-core";
 import { useEffect, useState } from "react";
 
+// to only be set once, even if each invocation useIpfsFactory() creates
+// a new instance of useIpfsFactory component hook
 let ipfs: Ipfs.IPFS | undefined = undefined;
 
-type useIpfsFactoryReturnT = {
+type UseIpfsFactoryReturn = {
   ipfs?: Ipfs.IPFS;
   isIpfsReady: boolean;
   ipfsInitError: string;
 };
 
-const useIpfsFactory = (): useIpfsFactoryReturnT => {
+export const useIpfsFactory = (): UseIpfsFactoryReturn => {
   const [isIpfsReady, setIpfsReady] = useState(false);
   const [ipfsInitError, setIpfsInitError] = useState<string>("");
 
@@ -17,35 +19,32 @@ const useIpfsFactory = (): useIpfsFactoryReturnT => {
     // The fn to useEffect should not return anything other than a cleanup fn,
     // So it cannot be marked async, which causes it to return a promise,
     // Hence we delegate to a async fn rather than making the param an async fn.
-    async function startIpfs() {
+    const startIpfs = async () => {
       if (ipfs) {
         console.log("IPFS already started");
         return;
-        //@ts-ignore
-      } else if (window.ipfs?.enable) {
+      }
+
+      //@ts-ignore
+      if (window.ipfs?.enable) {
         console.log("Found window.ipfs");
         //@ts-ignore
         ipfs = window.ipfs;
-      } else {
-        try {
-          ipfs = await Ipfs.create();
-          // const lsFiles = _ipfs.files.ls(
-          //   "/ipfs/QmRBh6trb2nEa9wFfMfYwcKyGyu8jSEtqRgkVH6UY6VnGa"
-          // );
-          // for (await blob of lsFiles) {
-          //   console.log(blob);
-          // }
+        return;
+      }
 
-          console.timeEnd("IPFS Started");
-        } catch (error) {
-          setIpfsInitError(error);
-        }
+      try {
+        ipfs = await Ipfs.create();
+        console.timeEnd("IPFS Started");
+      } catch (error) {
+        setIpfsInitError(error);
       }
 
       setIpfsReady(Boolean(ipfs));
-    }
+    };
 
     startIpfs();
+
     return function cleanup() {
       if (ipfs?.stop) {
         console.log("Stopping IPFS");
