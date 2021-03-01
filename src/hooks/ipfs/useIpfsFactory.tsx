e45e846db952e9cec1141fb2b0d8d74f92fc5976
naz <nazariy@inbox.ru>
2021-03-01 16:52:50 +0000

    [+] useIpfsFactory clean up

--- src/hooks/ipfs/useIpfsFactory.tsx
+++ src/hooks/ipfs/useIpfsFactory.tsx
@@ -1,7 +1,7 @@
 import Ipfs from "ipfs-core";
 import { useEffect, useState } from "react";
 
-// const ipfs: Ipfs.IPFS | null = null;
+let ipfs: Ipfs.IPFS | undefined = undefined;
 
 type useIpfsFactoryReturnT = {
   ipfs?: Ipfs.IPFS;
@@ -9,20 +9,9 @@ type useIpfsFactoryReturnT = {
   ipfsInitError: string;
 };
 
-/*
- * A quick demo using React hooks to create an ipfs instance.
- *
- * Hooks are brand new at the time of writing, and this pattern
- * is intended to show it is possible. I don't know if it is wise.
- *
- * Next steps would be to store the ipfs instance on the context
- * so use-ipfs calls can grab it from there rather than expecting
- * it to be passed in.
- */
 const useIpfsFactory = (): useIpfsFactoryReturnT => {
   const [isIpfsReady, setIpfsReady] = useState(false);
   const [ipfsInitError, setIpfsInitError] = useState<string>("");
-  const [ipfs, setIpfs] = useState<Ipfs.IPFS>();
 
   useEffect(() => {
     // The fn to useEffect should not return anything other than a cleanup fn,
@@ -36,15 +25,10 @@ const useIpfsFactory = (): useIpfsFactoryReturnT => {
       } else if (window.ipfs?.enable) {
         console.log("Found window.ipfs");
         //@ts-ignore
-        const _ipfs = await window.ipfs.enable({ commands: ["id"] });
-        setIpfs(_ipfs);
+        ipfs = window.ipfs;
       } else {
         try {
-          console.time("IPFS Started");
-          const _ipfs = await Ipfs.create();
-          console.log("_ipfs", _ipfs);
-          setIpfs(_ipfs);
-
+          ipfs = await Ipfs.create();
           // const lsFiles = _ipfs.files.ls(
           //   "/ipfs/QmRBh6trb2nEa9wFfMfYwcKyGyu8jSEtqRgkVH6UY6VnGa"
           // );
@@ -54,8 +38,6 @@ const useIpfsFactory = (): useIpfsFactoryReturnT => {
 
           console.timeEnd("IPFS Started");
         } catch (error) {
-          console.error("IPFS init error:", error);
-          setIpfs(undefined);
           setIpfsInitError(error);
         }
       }
@@ -68,7 +50,7 @@ const useIpfsFactory = (): useIpfsFactoryReturnT => {
       if (ipfs?.stop) {
         console.log("Stopping IPFS");
         ipfs.stop().catch((err) => console.error(err));
-        setIpfs(undefined);
+        ipfs = undefined;
         setIpfsReady(false);
       }
     };
