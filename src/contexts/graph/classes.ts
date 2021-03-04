d5c74e1c2fbc42e0a32e086e9dbc45e2935ac666
naz <nazariy@inbox.ru>
2021-03-04 18:56:51 +0000

    [+] Nft partially implemented (ipfs left out)

--- src/contexts/graph/classes.ts
+++ src/contexts/graph/classes.ts
@@ -8,46 +8,81 @@ import {
   Renting as RentingType,
 } from "./types";
 import { parseLending, parseRenting } from "./utils";
+import { ethers } from "ethers";
+import { ERC721__factory } from "../../hardhat/typechain/factories/ERC721__factory";
+import { ERC1155__factory } from "../../hardhat/typechain/factories/ERC1155__factory";
 
 type NftOptions = {
   tokenURI?: string;
 };
 
 class Nft {
-  constructor(nftAddress: Address, tokenId: string, options?: NftOptions) {
+  constructor(
+    nftAddress: Address,
+    tokenId: string,
+    signer: ethers.Signer,
+    options?: NftOptions
+  ) {
     this.address = nftAddress;
     this.tokenId = tokenId;
+    this.signer = signer;
+    this.isERC721 = false;
 
     if (options?.tokenURI) {
       this._tokenURI = options?.tokenURI;
     }
   }
+
   address: Address;
   tokenId: string;
+  signer: ethers.Signer;
+  isERC721: boolean;
   _tokenURI: string | undefined;
+  _contract: ERC721 | ERC1155 | undefined;
+
   contract = (): ERC721 | ERC1155 => {
-    return {} as ERC721;
+    if (this._contract) return this._contract;
+    // todo: not exactly sure if this will work
+    try {
+      const __contract = ERC721__factory.connect(this.address, this.signer);
+      this.isERC721 = true;
+      this._contract = __contract;
+      return __contract;
+    } catch {
+      const __contract = ERC1155__factory.connect(this.address, this.signer);
+      this._contract = __contract;
+      return __contract;
+    }
   };
+
   tokenURI = async (): Promise<string | undefined> => {
     if (this._tokenURI) return this._tokenURI;
-    return "";
+    if (!this._contract) this.contract();
+    if (this.isERC721) {
+      return await this.contract().tokenURI();
+    } else {
+      return `${await this.contract().uri()}/${this.tokenId}`;
+    }
   };
 
   // todo: look at the meta definition
   // todo: https://github.com/ethereum/eips/issues/721#issuecomment-343246872
   // todo: there is another link, not this one
+  // todo: also, we need this https://github.com/0xsequence/collectible-lists
   /**
    * This is parsed tokenURI is JSON
    */
   meta = async (): Promise<any> => {
     return {};
   };
+
   /**
    * This is image from meta or from tokenURI. Sometimes,
    * tokenURI directly gives a link to media, instead of
    * meta...
    */
   mediaURI = async (): Promise<string | undefined> => {
+    // todo: ipfs
     return "";
   };
 }
@@ -56,10 +91,11 @@ class Lending extends Nft {
   constructor(
     nftAddress: Address,
     tokenId: string,
+    signer: ethers.Signer,
     lendingRaw: LendingRaw,
     options?: NftOptions
   ) {
-    super(nftAddress, tokenId, options);
+    super(nftAddress, tokenId, signer, options);
 
     this.lending = parseLending(lendingRaw);
     this.id = lendingRaw.id;
@@ -72,10 +108,11 @@ class Renting extends Nft {
   constructor(
     nftAddress: Address,
     tokenId: string,
+    signer: ethers.Signer,
     rentingRaw: RentingRaw,
     options?: NftOptions
   ) {
-    super(nftAddress, tokenId, options);
+    super(nftAddress, tokenId, signer, options);
 
     this.renting = parseRenting(rentingRaw);
     this.id = rentingRaw.id;
