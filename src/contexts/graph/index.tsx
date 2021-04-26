7742dc647f7e3833c138f50da4975be8f2b0f8fb
nazariyv <nazariy@inbox.ru>
2021-04-26 12:42:40 +0100

    improved nft loading

--- src/contexts/graph/index.tsx
+++ src/contexts/graph/index.tsx
@@ -187,8 +187,6 @@ export const GraphProvider: React.FC = ({ children }) => {
     const usersNfts721 = await fetchUserProd(FetchType.ERC721);
     const usersNfts1155 = await fetchUserProd(FetchType.ERC1155);
     const _usersNfts = usersNfts721.concat(usersNfts1155).map((nft) => {
-      console.log("nft", nft);
-
       return new Nft(nft.address, nft.tokenId, nft.isERC721, signer, {
         meta: nft.meta,
         tokenURI: nft.tokenURI,
--- src/services/fetch-nft-meta.ts
+++ src/services/fetch-nft-meta.ts
@@ -4,41 +4,78 @@ import { CORS_PROXY } from "../consts";
 
 const IPFSGateway = "http://dweb.link/ipfs/";
 
-const isIpfsUrl = (url: string) => {
-  return (
-    /^(\/ipfs|ipfs:\/)\/Qm[1-9A-HJ-NP-Za-km-z]{44}$/.test(url) ||
-    url.startsWith("ipfs://ipfs/")
-  );
+/**
+ * Matches IPFS CIDv0 (all start with Qm)
+ * Matches IPFS CIDv1 (all start with b and use base32 case-insensitive encoding)
+ * @param url
+ * @returns
+ */
+const matchIPFS_URL = (url: string) => {
+  const isIPFS_URL =
+    url.match(/(Qm[1-9A-HJ-NP-Za-km-z]{44})(\/.+)?$/) ||
+    url.match(/(b[1-7a-zA-Z]{58})(\/.+)?$/);
+  return isIPFS_URL;
 };
 
-// TODO: will not match the new format that starts with a char 'b'
-const matchIPFS = (url: string) =>
-  url.match(/(Qm[1-9A-HJ-NP-Za-km-z]{44})(\/.+)?$/);
-
-// there are issues with CORS policy. So Sandbox and a couple of others need to be
-// requested through a proxy
-const matchSandbox = (url: string) =>
-  url.match(/^(https:\/\/api.sandbox.game\/lands)/);
-
-const buildStaticIPFS_URL = (url: string) => {
-  const ipfsMatch = matchIPFS(url);
-  if (ipfsMatch) {
-    const [, cid, path = ""] = ipfsMatch;
-    return `${IPFSGateway}${cid}${path}`;
-  }
+const matchWeirdBaseURL = (url: string) => {
+  const isWeird = url.endsWith("0x{id}");
+  return isWeird;
 };
 
-const loadMetaFromIPFS = async (url: string): Promise<NftToken["meta"]> => {
-  const ipfsMatch = matchIPFS(url);
-  // * the if is required to satisfy the typescript. Otherwise, it may think it is null
-  if (!ipfsMatch) return {};
-  const [, cid, path = ""] = ipfsMatch;
-  try {
-    const response = await fetch(`${IPFSGateway}${cid}${path}`);
-    const data = await response.json();
+const removeWeirdBaseURLEnd = (url: string) => {
+  const withoutEnd = url.slice(0, url.length - "0x{id}".length);
+  return withoutEnd;
+};
+
+/**
+ * ! There are issues with CORS policy. So Sandbox and a couple of others need to be
+ * ! requested through a proxy
+ * @param url check if it is sandbox. Sandbox does not set CORS headers, and so you need
+ * to proxy the request, unfortunately.
+ * @returns
+ */
+const isSandbox = (url: string) =>
+  /^(https:\/\/api.sandbox.game\/lands)/.test(url);
+
+const buildStaticIPFS_URL = (matched: string[]) => {
+  const [, cid, path = ""] = matched;
+  return `${IPFSGateway}${cid}${path}`;
+};
+
+/**
+ *
+ * @param IPFS_URL is an output from matchIPFS_URL function.
+ * @returns
+ */
+const loadMetaFromIPFS = async (
+  IPFS_URL: RegExpMatchArray | null
+): Promise<NftToken["meta"]> => {
+  if (!IPFS_URL) {
+    console.warn("could not fetch meta IPFS URL");
     return {
-      image: isIpfsUrl(data?.image)
-        ? buildStaticIPFS_URL(data?.image)
+      image: "",
+      description: "",
+      name: "",
+    };
+  }
+
+  const staticIPFS_URL = buildStaticIPFS_URL(IPFS_URL);
+  try {
+    const response = await fetch(staticIPFS_URL);
+
+    let data: any = {};
+    try {
+      data = await response.json();
+    } catch (e) {
+      // ! this happens with ZORA media for me
+      console.warn("could not get json, which could mean this is media");
+      return { image: staticIPFS_URL };
+    }
+
+    const imageIsIPFS_URL = matchIPFS_URL(data?.image);
+    return {
+      image: imageIsIPFS_URL
+        ? buildStaticIPFS_URL(imageIsIPFS_URL)
         : data?.image,
       description: data?.description,
       name: data?.name,
@@ -50,39 +87,46 @@ const loadMetaFromIPFS = async (url: string): Promise<NftToken["meta"]> => {
 
 export const fetchNFTMeta = async (nft: Nft): Promise<NftToken["meta"]> => {
   const { _mediaURI, _tokenURI } = nft;
-  if (_mediaURI) return { image: _mediaURI };
 
-  if (_tokenURI) {
-    if (isIpfsUrl(_tokenURI)) {
-      return await loadMetaFromIPFS(_tokenURI);
-    } else {
-      try {
-        // ! name this flag "proxyable".
-        // ! if isProxyable, then use firebase cors proxy.
-        // ! so far (my wallet), it appears that only Sandbox requires proxying.
-        // ! people will tell us: my X NFT is not showing. We will check, and it
-        // ! will probably because we aren't proxying the request for meta here
-        const isSandbox = matchSandbox(_tokenURI);
-        const fetchThis = isSandbox ? `${CORS_PROXY}${_tokenURI}` : _tokenURI;
-        const response = await fetch(fetchThis);
-        console.log("response", response);
-        const data = await response?.json();
-        if (!data?.image?.startsWith("ipfs://ipfs/")) {
-          return {
-            image: data?.image,
-            description: data?.description,
-            name: data?.name,
-          };
-        } else {
-          return await loadMetaFromIPFS(_tokenURI);
-        }
-      } catch (err) {
-        console.warn(err);
-      }
-    }
+  let tokenURI: string = _tokenURI;
 
-    return {};
+  const isWeirdBaseURL = matchWeirdBaseURL(tokenURI);
+  if (isWeirdBaseURL) {
+    // ! this is opensea, in my tests. And even though this weird base url says you need hex
+    // ! form int, you should in fact, pass an int number lol...
+    tokenURI = removeWeirdBaseURLEnd(tokenURI) + nft.tokenId;
   }
 
-  return {};
+  if (_mediaURI) {
+    return { image: _mediaURI };
+  }
+  if (!tokenURI) return {};
+
+  const isIPFS_URL = matchIPFS_URL(tokenURI);
+  if (isIPFS_URL) return await loadMetaFromIPFS(isIPFS_URL);
+
+  try {
+    // ! people will tell us: my X NFT is not showing. We will check, and it
+    // ! will probably because we aren't proxying the request for meta here
+    const isProxyable = isSandbox(tokenURI);
+    const fetchThis = isProxyable ? `${CORS_PROXY}${tokenURI}` : tokenURI;
+    const response = await fetch(fetchThis);
+    const data = await response?.json();
+
+    if (!data?.image?.startsWith("ipfs://ipfs/")) {
+      return {
+        image: data?.image,
+        description: data?.description,
+        name: data?.name,
+      };
+    } else {
+      console.warn(
+        "is not IPFS URL, but we are downloading meta as if it is O_O"
+      );
+      return {};
+    }
+  } catch (err) {
+    console.warn(err);
+    return {};
+  }
 };
