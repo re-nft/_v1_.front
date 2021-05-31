8fe220acce7532adce2c6c1c37692261a815a97d
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2021-05-31 17:51:20 +0700

    dashboard changes (#49)
    * dashboard changes
    
    * format

---
+++ src/hooks/useTimestamp.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useContext, useEffect, useState } from "react";
+import { ProviderContext } from "../hardhat/SymfoniContext";
+import { Block } from "@ethersproject/abstract-provider";
+import createDebugger from "debug";
+import usePoller from "./usePoller";
+
+const debug = createDebugger("app:contracts:blocks");
+
+const day = 24 * 60 * 60 * 1000;
+
+export const useTimestamp = (): number => {
+  const [provider] = useContext(ProviderContext);
+  const [timeStamp, setTimestamp] = useState(Date.now() - day);
+
+  const getTimestamp = useCallback(() => {
+    if (provider) {
+      return provider
+        .getBlock("latest")
+        .then((block: Block) => {
+          console.log(new Date(block.timestamp));
+
+          if (timeStamp !== block.timestamp) setTimestamp(block.timestamp);
+        })
+        .catch((e) => {
+          if (e) debug(e);
+        });
+    }
+    return Promise.reject();
+  }, [provider, timeStamp]);
+
+  useEffect(() => {
+    getTimestamp();
+  }, [getTimestamp, timeStamp]);
+
+  usePoller(() => {
+    getTimestamp();
+  }, 2000);
+
+  return timeStamp;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,21 @@ if (process.env.REACT_APP_DEBUG) {
 import App from "./components/app-layout";
 import { StateProvider } from "./contexts/StateProvider";
 
+import { ErrorBoundary } from "react-error-boundary";
+
+const ErrorFallback: React.FC<{
+  error: Error;
+  resetErrorBoundary: () => void;
+}> = ({ error, resetErrorBoundary }) => {
+  return (
+    <div>
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Click to refresh</button>
+    </div>
+  );
+};
+
 const theme = createMuiTheme({
   typography: {
     fontFamily: [
@@ -29,7 +44,15 @@ const theme = createMuiTheme({
 ReactDOM.render(
   <StateProvider>
     <ThemeProvider theme={theme}>
-      <App />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          // TODO we will do something better, refresh for now
+          window.location.reload();
+        }}
+      >
+        <App />
+      </ErrorBoundary>
     </ThemeProvider>
   </StateProvider>,
   document.getElementById("root")
