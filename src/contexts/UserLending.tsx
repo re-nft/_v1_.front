4ec25e10a2231ed38823c7655208f224f71b5552
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2021-06-14 18:40:50 +0700

    fix claim showing up claimed

--- src/contexts/UserLending.tsx
+++ src/contexts/UserLending.tsx
@@ -58,6 +58,7 @@ export const UserLendingProvider: React.FC = ({ children }) => {
         if (response && response.users && response.users[0]) {
           const lendings = Object.values(response.users[0].lending)
             .filter((v) => v != null)
+            .filter((v) => !v.collateralClaimed)
             .map((lending) => {
               return new Lending(lending, signer);
             });
