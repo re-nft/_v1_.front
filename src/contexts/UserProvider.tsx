12c3cb1083a7d8dcd49884ca465476f0fbfe9b02
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2021-06-17 15:15:55 +0700

    refactor buggy connect/disconnect

--- src/contexts/UserProvider.tsx
+++ src/contexts/UserProvider.tsx
@@ -8,6 +8,8 @@ import React, {
 import { ethers, Signer } from "ethers";
 import Web3Modal from "web3modal";
 import { THROWS } from "../utils";
+import usePoller from "../hooks/usePoller";
+import createCancellablePromise from "./create-cancellable-promise";
 
 const DefaultUser = {
   address: "",
@@ -37,51 +39,107 @@ export const UserProvider: React.FC = ({ children }) => {
     useState<ethers.providers.Web3Provider>();
   const [signer, setSigner] = useState<Signer>();
   const [address, setAddress] = useState<string>("");
+  const [permissions, setPermissions] = useState<unknown[]>([]);
 
   const providerOptions = useMemo(() => ({}), []);
   // web3modal is only working in browser\]
   const web3Modal = useMemo(() => {
     return typeof window !== "undefined"
       ? new Web3Modal({
-          cacheProvider: true,
+          cacheProvider: false,
           providerOptions, // required
         })
       : null;
   }, [providerOptions]);
 
-  const connect = useCallback(async () => {
-    if (web3Modal) {
-      const provider = await web3Modal
-        .connect()
-        .then((p) => p)
-        .catch(() => {
-          // do nothing
-        });
-      if (!provider) return;
-      const web3p = new ethers.providers.Web3Provider(provider);
-      const network = await web3p?.getNetwork();
-      const name = network.chainId === 31337 ? "local" : network?.name;
-      setNetworkName(name);
-      const _signer = web3p.getSigner();
-      setSigner(_signer);
-      const address = await _signer
-        .getAddress()
-        .then((t) => t.toLowerCase())
-        .catch(() => {
-          // do nothing
-        });
-      setAddress(address || "");
-      setProvider(provider);
-      setWeb3Provider(web3p);
-      return Promise.resolve(web3p);
-    }
-  }, [web3Modal]);
+  const initState = useCallback(async (provider) => {
+    const web3p = new ethers.providers.Web3Provider(provider);
+    const network = await web3p?.getNetwork();
+    const name = network.chainId === 31337 ? "local" : network?.name;
+    setNetworkName(name);
+    const _signer = web3p.getSigner();
+    setSigner(_signer);
+    const address = await _signer
+      .getAddress()
+      .then((t) => t.toLowerCase())
+      .catch(() => {
+        // do nothing
+      });
+    setAddress(address || "");
+    setProvider(provider);
+    setWeb3Provider(web3p);
+    return Promise.resolve(web3p);
+  }, []);
 
-  useEffect(() => {
-    if (web3Modal && web3Modal.cachedProvider) {
-      connect();
-    }
-  }, [connect, web3Modal]);
+  const connect = useCallback(
+    async (manual: boolean) => {
+      if (web3Modal) {
+        // only reconnect if we have permissions or
+        // user manually connected through action
+        if (!!manual || (permissions.length > 0 && !signer)) {
+          const provider = await web3Modal
+            .connect()
+            .then((p) => p)
+            .catch(() => {
+              // do nothing
+            });
+          if (!provider) return;
+          await initState(provider);
+        }
+      }
+    },
+    [web3Modal, permissions, signer, initState]
+  );
+
+  // there is no better way to do disconnect with metemask+web3modal combo
+  const connectDisconnect = useCallback(() => {
+    const request = createCancellablePromise<unknown[]>(
+      window.ethereum.request({ method: "wallet_getPermissions" })
+    );
+
+    request.promise
+      .then((permissions: unknown[]) => {
+        setPermissions(permissions);
+        if (permissions.length < 1) {
+          if (signer) setSigner(undefined);
+          if (address) setAddress("");
+          if (network) setNetworkName("");
+        }
+      })
+      .catch((error: unknown) => {
+        console.warn(error)
+      });
+    return request.cancel;
+  }, [address, network, signer]);
+
+  usePoller(connectDisconnect, 2000);
+  usePoller(() => {
+    connect(false);
+  }, 2000);
+
+  const manuallyConnect = useCallback(() => {
+    connect(true);
+  }, [connect]);
+
+  const accountsChanged = useCallback(
+    (arg) => {
+      if (arg.lengths > 0) connect(false);
+      if (arg.lengts === 0) {
+        // disconnect case
+        if (permissions.length > 0) setPermissions([]);
+        if (signer) setSigner(undefined);
+        if (address) setAddress("");
+        if (network) setNetworkName("");
+      }
+    },
+    [address, connect, network, permissions.length, signer]
+  );
+  const chainChanged = useCallback(
+    (arg) => {
+      connect(false);
+    },
+    [connect]
+  );
 
   // change account
   useEffect(() => {
@@ -90,25 +148,33 @@ export const UserProvider: React.FC = ({ children }) => {
       // web3modal is untyped...
       // https://github.com/Web3Modal/web3modal/blob/2ff929d0e99df5edf6bb9e88cff338ba6d8a3991/src/core/index.tsx#L71
       // @ts-ignore
-      provider.on("accountsChanged", connect);
+      provider.on("accountsChanged", accountsChanged);
       // @ts-ignore
-      provider.on("chainChanged", connect);
+      provider.on("chainChanged", chainChanged);
     }
     return () => {
-      if (provider) {
+      // @ts-ignore
+      if (provider && typeof provider.off !== "undefined") {
         // web3modal is untyped...
         // https://github.com/Web3Modal/web3modal/blob/2ff929d0e99df5edf6bb9e88cff338ba6d8a3991/src/core/index.tsx#L71
         // @ts-ignore
-        provider.off("accountsChanged", connect);
+        provider.off("accountsChanged", accountsChanged);
         // @ts-ignore
-        provider.off("chainChanged", connect);
+        provider.off("chainChanged", chainChanged);
       }
     };
-  }, [connect, provider]);
+  }, [accountsChanged, chainChanged, provider]);
 
   return (
     <UserContext.Provider
-      value={{ connect, provider, signer, address, web3Provider, network }}
+      value={{
+        connect: manuallyConnect,
+        provider,
+        signer,
+        address,
+        web3Provider,
+        network,
+      }}
     >
       {children}
     </UserContext.Provider>
