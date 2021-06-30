5baca1c75eb54b3500a143f9e3d03e87e04771ac
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2021-06-30 13:39:36 +0700

    remove dead code

--- src/utils.ts
+++ src/utils.ts
@@ -148,32 +148,6 @@ export const unpackPrice = (price: BigNumberish): number => {
   // return Number(_price) / divide;
 };
 
-export const packPrice = (price: number): string => {
-  if (price > 9999.9999) throw new Error("too high");
-  if (price < 0.0001) throw new Error("too low");
-  const stringVersion = price.toString();
-  const parts = stringVersion.split(".");
-  let res: string;
-  if (parts.length == 2) {
-    const whole = parts[0];
-    let decimal = parts[1];
-    while (decimal.length < 4) {
-      decimal += "0";
-    }
-    const wholeHex = decimalToPaddedHexString(Number(whole), 16);
-    const decimalHex = decimalToPaddedHexString(Number(decimal), 16);
-    const hexRepr = wholeHex.concat(decimalHex.slice(2));
-    res = hexRepr;
-  } else {
-    if (parts.length != 1) throw new Error("price packing issue");
-    const whole = parts[0];
-    const wholeHex = decimalToPaddedHexString(Number(whole), 16);
-    const decimalHex = "0000";
-    res = wholeHex.concat(decimalHex);
-  }
-  return res;
-};
-
 // ! must be the same as in packages/contracts/src/interfaces/IResolver.sol
 export const parsePaymentToken = (tkn: string): PaymentToken => {
   switch (tkn) {
