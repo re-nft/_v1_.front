7a0017159221a52b1995ed7216218031687b9156
nazariyv <nazariy@inbox.ru>
2021-07-23 13:16:23 +0100

    faq: how to rent

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -103,18 +103,80 @@ const ButHow: React.FC = () => {
       <div style={{ marginBottom: "1em" }}>
         <img src="assets/faq/how-to-rent-an-nft.png" />
       </div>
-      <p>
-        We will ask the borrower to pay the price you indicated in (i) if they
-        want to rent your NFT. This means that if they do not return your NFT,
-        you get back this money. As with everything in life, all good things
-        must come to an end. So we will enforce the borrower to return your NFT
-        before the end of their picked rent duration (which must not exceed what
-        you have set in (ii)). If they miss their deadline, the price they have
-        paid into our escrow in (i) will be automatically sent to you. As for
-        the last point (iii), this is how much the borrower will pay you every
-        day that they are renting your NFT
-      </p>
-      <img src="assets/faq/what-are-the-risks.png" />
+      <div>
+        Head out to the RENT tab above. Pick an NFT you would like to rent. You
+        will get a modal like this. Let&apos;s go through it together to
+        understand what each piece of information here means.
+        <div style={{ marginTop: "1em", textAlign: "center" }}>
+          <img src="assets/faq/rent-modal.png" />
+        </div>
+      </div>
+      <div>
+        <img src="assets/faq/bullet-1.png" />
+        &nbsp;This is the NFT standard. There are 2 main standards out there:
+        721 and 1155. The 1155 standard lets you mint semi-fungible NFTs, which
+        means that you have more than one NFT that looks the same, even though,
+        they would have the same id.
+      </div>
+      <br />
+      <div>
+        <img src="assets/faq/bullet-2.png" />
+        &nbsp;NFT address of the NFT you are about to rent.
+      </div>
+      <br />
+      <div>
+        <img src="assets/faq/bullet-3.png" />
+        &nbsp;Token ID of the NFT you are about to rent.
+      </div>
+      <br />
+      <div>
+        <img src="assets/faq/bullet-4.png" />
+        &nbsp;For 1155 standard, this number will tell you how many copies of
+        this semi-fungible NFT the lender is lending. The collateral you will
+        pay will be multiplied by this number.
+      </div>
+      <br />
+      <div>
+        <img src="assets/faq/bullet-5.png" />
+        &nbsp;The length of the rent. If you fail to return the NFT by the end
+        of this period, the lender will be able to claim the collateral you will
+        pay.
+      </div>
+      <br />
+      <div>
+        <img src="assets/faq/bullet-6.png" />
+        &nbsp;Total daily rent price for all the NFTs.
+      </div>
+      <br />
+      <div>
+        <img src="assets/faq/bullet-7.png" />
+        &nbsp;Collateral price per NFT. If this is a 721 standard, then this is
+        a total collateral you have to pay. If this is 1155, then this is a per
+        item collateral price. Note that you will have to pay full rent amount
+        plus full collateral to rent. If you return earlier, the rent payment
+        will be compensated proportionally. Collateral gets returned on
+        successful return, as well.
+      </div>
+      <br />
+      <div>
+        <img src="assets/faq/bullet-8.png" />
+        &nbsp;Depending on the standard and the number of days of the rent, this
+        field will give you the total amount payable you will send to the reNFT
+        contract for escrow.
+      </div>
+      <br />
+      <div>
+        <img src="assets/faq/bullet-9.png" />
+        &nbsp;If this is the first time you are renting, you will need to
+        approve the payment token for reNFT. You will only need to do this once,
+        per payment token. This button will change to Rent, once you have
+        approved the payment token.
+      </div>
+      <br />
+      <br />
+      <div style={{ marginBottom: "1em" }}>
+        <img src="assets/faq/what-are-the-risks.png" />
+      </div>
       <br />
       The contracts were not audited. However, they have been thoroughly tested
       and peer reviewed, exercise caution, all responsibility due to loss bares
