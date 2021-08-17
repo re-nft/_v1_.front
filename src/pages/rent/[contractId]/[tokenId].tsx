72cf61dae688091dec9cd1dc3fd5ffbe179c2911
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2021-08-17 15:37:05 +0700

    disable imageUrl

--- src/pages/rent/[contractId]/[tokenId].tsx
+++ src/pages/rent/[contractId]/[tokenId].tsx
@@ -3,13 +3,13 @@ import React, { useMemo, useCallback, useEffect } from "react";
 import { AvailableToRent } from "../../../components/pages/available-to-rent";
 import { RentSwitchWrapper } from "../../../components/rent-switch-wrapper";
 import { useAllAvailableForRent } from "../../../hooks/useAllAvailableForRent";
-import Head from "next/head";
-import { useFetchMeta, useNftMetaState } from "../../../hooks/useMetaState";
-import shallow from "zustand/shallow";
+// import Head from "next/head";
+// import { useFetchMeta, useNftMetaState } from "../../../hooks/useMetaState";
+// import shallow from "zustand/shallow";
 
 const AvailableToRentPage: React.FC = () => {
   const { allAvailableToRent, isLoading } = useAllAvailableForRent();
-  const fetchMeta = useFetchMeta();
+  //const fetchMeta = useFetchMeta();
 
   const {
     query: { contractId, tokenId },
@@ -25,20 +25,20 @@ const AvailableToRentPage: React.FC = () => {
     return match ? [match] : [];
   }, [match]);
 
-  const imageURL = useNftMetaState(
-    useCallback(
-      (state) => {
-        const id = all[0]?.nId;
-        return id && state.metas ? state.metas[id]?.image : "";
-      },
-      [all]
-    ),
-    shallow
-  );
+  // const imageURL = useNftMetaState(
+  //   useCallback(
+  //     (state) => {
+  //       const id = all[0]?.nId;
+  //       return id && state.metas ? state.metas[id]?.image : "";
+  //     },
+  //     [all]
+  //   ),
+  //   shallow
+  // );
 
-  useEffect(() => {
-    fetchMeta(all);
-  }, [all, fetchMeta]);
+  // useEffect(() => {
+  //   fetchMeta(all);
+  // }, [all, fetchMeta]);
 
   if (!match && !isLoading)
     return (
@@ -50,10 +50,10 @@ const AvailableToRentPage: React.FC = () => {
     );
   return (
     <>
-      <Head>
+      {/* <Head>
         <meta property="twitter:image" key="twitter:image" content={imageURL} />
         <meta property="og:image" content={imageURL} key="og:image" />
-      </Head>
+      </Head> */}
 
       <AvailableToRent isLoading={isLoading} allAvailableToRent={all} />
     </>
