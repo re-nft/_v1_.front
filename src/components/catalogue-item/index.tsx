454c50094e39e58f0cde46c4e07d53cd20b296a2
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2021-09-17 14:53:19 +0700

    remove flip-toolkit

--- src/components/catalogue-item/index.tsx
+++ src/components/catalogue-item/index.tsx
@@ -8,7 +8,6 @@ import { CatalogueItemDisplay } from "./catalogue-item-display";
 import { useRouter } from "next/router";
 import { useNftMetaState } from "../../hooks/queries/useMetaState";
 import shallow from "zustand/shallow";
-import { Flipped, spring } from "react-flip-toolkit";
 import { CopyLink } from "../copy-link";
 import { ShortenPopover } from "../common/shorten-popover";
 import { CatalogueActions } from "./catalogue-actions";
@@ -22,40 +21,13 @@ export type CatalogueItemProps = {
   disabled?: boolean;
 };
 
-const onElementAppear = (el: HTMLElement, index: number) =>
-  spring({
-    onUpdate: (val) => {
-      el.style.opacity = val.toString();
-    },
-    delay: index * 50,
-  });
-
-const onExit =
-  (type: "grid" | "list") =>
-  (el: HTMLElement, index: number, removeElement: () => void) => {
-    spring({
-      config: { overshootClamping: true },
-      onUpdate: (val) => {
-        el.style.transform = `scale${type === "grid" ? "X" : "Y"}(${
-          1 - Number(val)
-        })`;
-      },
-      delay: index * 50,
-      onComplete: removeElement,
-    });
-
-    return () => {
-      el.style.opacity = "";
-      removeElement();
-    };
-  };
 //TODO:eniko make this component accept nId instead of Nft
 export const CatalogueItem: React.FC<CatalogueItemProps> = ({
   nft,
   checked,
   onCheckboxChange,
   children,
-  disabled,
+  disabled
 }) => {
   const { signer } = useWallet();
   const { pathname } = useRouter();
@@ -89,114 +61,95 @@ export const CatalogueItem: React.FC<CatalogueItemProps> = ({
 
   const knownContract = useMemo(() => {
     return (
-      nft.nftAddress.toLowerCase() === "0x0db8c099b426677f575d512874d45a767e9acc3c"
+      nft.nftAddress.toLowerCase() ===
+      "0x0db8c099b426677f575d512874d45a767e9acc3c"
     );
   }, [nft.nftAddress]);
 
   return (
-    <Flipped
+    <div
       key={nft.id}
-      flipId={nft.id}
-      onAppear={onElementAppear}
-      onExit={onExit("grid")}
-      stagger={true}
+      className={`text-base leading-tight flex flex-col bg-white border-4 border-black hover:shadow-rn-one pb-1 ${
+        checked ? "shadow-rn-one" : ""
+      }`}
+      data-item-id={nft.tokenId}
     >
-      <div
-        className={`text-base leading-tight flex flex-col bg-white border-4 border-black hover:shadow-rn-one pb-1 ${
-          checked ? "shadow-rn-one" : ""
-        }`}
-        key={nft.tokenId}
-        data-item-id={nft.tokenId}
-      >
-        {!imageIsReady && <Skeleton />}
-        {imageIsReady && (
+      {!imageIsReady && <Skeleton />}
+      {imageIsReady && (
+        <>
           <>
-            <Flipped
-              flipId={`${nft.id}-content`}
-              translate
-              shouldFlip={shouldFlip}
-              delayUntil={nft.id}
-            >
-              <>
-                <div className="flex justify-center space-x-2">
+            <div className="flex justify-center space-x-2">
+              <a
+                className="flex-initial"
+                target="_blank"
+                rel="noreferrer"
+                href={`https://rarible.com/token/${nft.nftAddress}:${nft.tokenId}`}
+              >
+                <img src="/assets/rarible.png" className="nft__icon" />
+              </a>
+              {openseaLink && (
+                <a
+                  className="flex-initial"
+                  target="_blank"
+                  rel="noreferrer"
+                  href={openseaLink}
+                >
+                  <img src="/assets/opensea.png" className="nft__icon" />
+                </a>
+              )}
+              <CatalogueActions
+                address={nft.nftAddress}
+                tokenId={nft.tokenId}
+              />
+              <div className="flex-1 flex justify-end justify-self-end">
+                <Checkbox
+                  checked={!!checked}
+                  onChange={onCheckboxChange}
+                  disabled={disabled || !signer}
+                ></Checkbox>
+              </div>
+            </div>
+            <CatalogueItemDisplay image={image} description={description} />
+            <div className="font-display text-xs leading-tight text-center py-3 px-4 flex flex-col justify-center items-center">
+              <p className="flex-initial">{name}</p>
+              <div className="flex flex-auto flex-row">
+                {knownContract && (
                   <a
-                    className="flex-initial"
+                    className="flex-initial p-2"
                     target="_blank"
                     rel="noreferrer"
-                    href={`https://rarible.com/token/${nft.nftAddress}:${nft.tokenId}`}
                   >
-                    <img src="/assets/rarible.png" className="nft__icon" />
+                    <img
+                      src="/assets/nft-verified.png"
+                      className="nft__icon small"
+                    />
                   </a>
-                  {openseaLink && (
-                    <a
-                      className="flex-initial"
-                      target="_blank"
-                      rel="noreferrer"
-                      href={openseaLink}
-                    >
-                      <img src="/assets/opensea.png" className="nft__icon" />
-                    </a>
-                  )}
-                  <CatalogueActions
-                    address={nft.nftAddress}
-                    tokenId={nft.tokenId}
-                  />
-                  <div className="flex-1 flex justify-end justify-self-end">
-                    <Checkbox
-                      checked={!!checked}
-                      onChange={onCheckboxChange}
-                      disabled={disabled || !signer}
-                    ></Checkbox>
-                  </div>
-                </div>
-                <CatalogueItemDisplay image={image} description={description} />
-                <div className="font-display text-xs leading-tight text-center py-3 px-4 flex flex-col justify-center items-center">
-                  <p className="flex-initial">{name}</p>
-                  <div className="flex flex-auto flex-row">
-                    {knownContract && (
-                      <a
-                        className="flex-initial p-2"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <img
-                          src="/assets/nft-verified.png"
-                          className="nft__icon small"
-                        />
-                      </a>
-                    )}
-                    {isRentPage && (
-                      <CopyLink address={nft.nftAddress} tokenId={nft.tokenId} />
-                    )}
-                  </div>
-                </div>
-              </>
-            </Flipped>
-            <Flipped
-              flipId={`${nft.id}-button`}
-              shouldFlip={shouldFlip}
-              delayUntil={nft.id}
-            >
-              <div className="px-2 flex flex-auto flex-col">
-                <CatalogueItemRow
-                  text="NFT Address"
-                  value={<ShortenPopover longString={nft.nftAddress} />}
-                />
-                <CatalogueItemRow
-                  text="Token id"
-                  value={<ShortenPopover longString={nft.tokenId} />}
-                />
-                <CatalogueItemRow
-                  text="Standard"
-                  value={nft.isERC721 ? "721" : "1155"}
-                />
-
-                {children}
+                )}
+                {isRentPage && (
+                  <CopyLink address={nft.nftAddress} tokenId={nft.tokenId} />
+                )}
               </div>
-            </Flipped>
+            </div>
           </>
-        )}
-      </div>
-    </Flipped>
+
+          <div className="px-2 flex flex-auto flex-col">
+            <CatalogueItemRow
+              text="NFT Address"
+              value={<ShortenPopover longString={nft.nftAddress} />}
+            />
+            <CatalogueItemRow
+              text="Token id"
+              value={<ShortenPopover longString={nft.tokenId} />}
+            />
+            <CatalogueItemRow
+              text="Standard"
+              value={nft.isERC721 ? "721" : "1155"}
+            />
+
+            {children}
+          </div>
+        </>
+      )}
+    </div>
   );
 };
--- src/components/common/items-wrapper.tsx
+++ src/components/common/items-wrapper.tsx
@@ -1,27 +1,14 @@
 import React from "react";
-import { Flipper } from "react-flip-toolkit";
 
 const ItemWrapper: React.FC<{
   flipId?: string | "flip";
 }> = ({ children, flipId }) => {
   return (
-    <Flipper
-      flipKey={flipId}
-      spring="wobbly"
-      staggerConfig={{
-        default: {
-          speed: 0.2,
-        },
-      }}
-      decisionData={{
-        stagger: "forward",
-        spring: "wobble",
-      }}
-    >
+    <div>
       <div className=" pt-8 grid grid-flow-row justify-items grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-8">
         {children}
       </div>
-    </Flipper>
+    </div>
   );
 };
 
