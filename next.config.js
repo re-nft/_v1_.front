38c17066faa49835c38dd2b35d107a304ba8f7fd
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2021-09-29 20:22:55 +0700

    add sentry for animetas tracking

--- next.config.js
+++ next.config.js
@@ -14,5 +14,23 @@ const nextConfig = {
 
 }
 
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-module.exports = withBundleAnalyzer(nextConfig)
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(withBundleAnalyzer(nextConfig), SentryWebpackPluginOptions)
